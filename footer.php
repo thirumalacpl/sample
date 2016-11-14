b08383728adfbfe65a01ed303cbe9ca408eae236
thirumalacpl <thirumal.acpl@gmail.com>
2016-11-14 12:48:26 +0530

    my sec changes

---
+++ footer.php
@@ -0,0 +1,136 @@
+<!-- ============= Footer ================ -->
+		<footer>
+			<div class="top_footer container-fluid">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-4 col-sm-12 part1">
+							<a href=""><img src="images/logo-footer.png" alt="Logo"></a>
+							<p>Welcome To The Force Of all the background verifications companies in India and their teams of
+part timers </p>
+							<p><i class="fa fa-phone"></i>&nbsp;&nbsp; (1800) 102 6149 </p>
+							<p>info@fourthforce.in</p>
+							<ul class="p0">
+								<li><a href=""><i class="fa fa-facebook"></i></a></li>
+								<li><a href=""><i class="fa fa-twitter"></i></a></li>
+								<li><a href=""><i class="fa fa-google-plus"></i></a></li>
+								<li><a href=""><i class="fa fa-linkedin"></i></a></li>
+								<li><a href=""><i class="fa fa-skype"></i></a></li>
+							</ul>
+						</div>
+						<div class="col-lg-2 col-md-3 col-sm-12 part2">
+							<h5>Our Services</h5>
+							<ul class="p0">
+								<li><a href=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Human Resource Criminal Risk Mitigation Verifications</a></li>
+								<li><a href=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Corporate Criminal Risk Mitigation Verifications</a></li>
+								<li><a href=""><i class="fa fa-angle-right"></i>&nbsp;&nbsp;Human Resource Criminal Risk Mitigation Verifications - Domestic</a></li>
+								
+							</ul>
+						</div>
+						<div class="col-lg-3 col-md-3 col-sm-12 part3">
+							<h5>Twitter Feeds</h5>
+							<div class="twitter"></div>							
+						</div>
+						<div class="col-lg-3 col-md-2 col-sm-12 part4">
+							<!-- <h5>Flickr Widget</h5>
+							<div class="gallery">
+								<img src="images/f-1.jpg" alt="image">
+								<img src="images/f-2.jpg" alt="image">
+								<img src="images/f-3.jpg" alt="image">
+								<img src="images/f-4.jpg" alt="image">
+								<img src="images/f-5.jpg" alt="image">
+								<img src="images/f-6.jpg" alt="image">
+							</div> -->
+
+
+							<style type="text/css">
+							.tag-bor {
+  font-size: 8pt;
+    display: inline-block;
+    margin-bottom: 6px;
+    margin-right: 2px;
+    padding: 4px 14px;
+    
+    border: solid 1px rgba(246, 246, 247, 0.36);
+    color: gray;
+}
+							</style>
+
+
+                            <h5>Information Tag</h5>
+							<div class="gallery">
+
+								<div class="tagcloud">
+<a href="#" class="tag-link-17 tag-link-position-1 tag-bor" title="Background Verification" style="font-size: 8pt;">Background Verification</a>
+<a href="#" class="tag-link-11 tag-link-position-3 tag-bor" title="FourthForce" style="font-size: 8pt;">FourthForce</a>
+<a href="#" title="1 topic" class="tag-bor" style="font-size: 8pt;">Cases</a>
+<a href="#" class="tag-link-14 tag-link-position-4 tag-bor" title="Services" style="font-size: 8pt;">Services</a>
+<a href="#" class="tag-link-10 tag-link-position-5 tag-bor" title="Insurance" style="font-size: 8pt;">Insurance</a>
+<a href="#" class="tag-link-20 tag-link-position-6 tag-bor" title="Criminal Cases" style="font-size: 8pt;">Criminal Cases</a>
+<a href="#" class="tag-link-21 tag-link-position-7 tag-bor" title="investigation" style="font-size: 8pt;">Investigation</a>
+<a href="#" class="tag-link-27 tag-link-position-8 tag-bor" title="Verify" style="font-size: 8pt;">Verify</a>
+=</div>
+							</div> 
+
+						</div>
+					</div> <!-- End row -->
+				</div>
+			</div> <!-- End top_footer -->
+			<div class="bottom_footer container-fluid">
+				<div class="container">
+					<p class="float_left">Copyright &copy; Fourthforce All rights reserved. </p>
+					<p class="float_right">Created by: <a href="http://www.advicesync-consulting.com/" style="color:  #315857;"> Advicesync</a></p>
+				</div>
+			</div> <!-- End bottom_footer -->
+		</footer>
+<!-- ============= /Footer =============== -->
+
+		<!-- Js File -->
+
+		<!-- j Query -->
+		<script type="text/javascript" src="js/jquery-2.1.4.js"></script>
+		<script type="text/javascript" src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+		<script type="text/javascript" src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+		<script type="text/javascript" src="js/jquery.bxslider.min.js"></script>
+		<script src="js/revolution-slider/jquery.themepunch.tools.min.js"></script> <!-- Revolution Slider Tools -->
+		<script src="js/revolution-slider/jquery.themepunch.revolution.min.js"></script> <!-- Revolution Slider -->
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.actions.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.carousel.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.kenburn.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.layeranimation.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.migration.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.navigation.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.parallax.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.slideanims.min.js"></script>
+		<script type="text/javascript" src="js/revolution-slider/extensions/revolution.extension.video.min.js"></script>
+
+		<!-- Bootstrap JS -->
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="js/jquery.appear.js"></script>
+		<script type="text/javascript" src="js/jquery.countTo.js"></script>
+		<script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
+		<!-- owl-carousel -->
+		<script type="text/javascript" src="js/owl.carousel.js"></script>
+		<script src="js/owl-custom.js"></script>
+		<!-- Custom & Vendor js -->
+		<script type="text/javascript" src="js/custom.js"></script>
+		
+<script type="text/javascript">
+$('.owl-carousel').owlCarousel({
+    loop:true,
+     margin:10,
+    nav:true,
+    responsive:{
+         0:{
+             items:1
+        },
+        600:{
+            items:3
+         },
+         1000:{
+             items:5
+         }
+     }
+ })
+ </script>
+	</body>
+</html>
